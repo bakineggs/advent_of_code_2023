3930f7e933f62145435adb05654b660683d6f2f8
Dan Barry <dan@bakineggs.com>
2024-01-01 16:49:33 -0800

    day 7 part 1 uses cards in order they're drawn for tie breaker

--- day_7/part_1.rb
+++ day_7/part_1.rb
@@ -2,8 +2,9 @@ CARD_RANK = Hash[['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2
 
 puts File.readlines(ARGV[0]).map {|line|
   raise "Failed to parse line #{line}" unless match = line.match(/^([#{CARD_RANK.keys.join}]{5}) (\d+)$/)
-  groups = match[1].chars.group_by {|c| c}.map {|rank, group| [5 - group.length, CARD_RANK[rank]]}.sort
-  [[groups.length, *groups], match[2].to_i]
+  cards = match[1].chars.map {|c| CARD_RANK[c]}
+  groups = cards.group_by {|c| c}.map {|_, group| 5 - group.length}.sort
+  [[groups.length, *groups, *cards], match[2].to_i]
 }.sort.reverse_each.each_with_index.sum {|(_, strength), rank|
   (rank + 1) * strength
 }
