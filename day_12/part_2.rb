b809d0096c2ce18a380e0baa3ccdd175a20cef93
Dan Barry <dan@bakineggs.com>
2024-01-08 16:04:01 -0800

    day 12 part 2

--- day_12/part_2.rb
+++ day_12/part_2.rb
@@ -1,34 +1,83 @@
+class DedupQueue
+  def initialize groups, lengths
+    @order = [[groups, lengths]]
+    @quantities = {[groups, lengths] => 1}
+  end
+
+  def empty?
+    @order.empty?
+  end
+
+  def pop
+    groups, lengths = @order.shift
+    quantity = @quantities[[groups, lengths]]
+    @quantities.delete [groups, lengths]
+    [groups, lengths, quantity]
+  end
+
+  def push groups, lengths, quantity
+    if @quantities.has_key? [groups, lengths]
+      @quantities[[groups, lengths]] += quantity
+    else
+      @quantities[[groups, lengths]] = quantity
+      @order.push [groups, lengths]
+    end
+  end
+end
+
 puts File.readlines(ARGV[0]).sum {|line|
   raise "Failed to parse line #{line}" unless match = line.match(/^([\.?#]+) (\d+(,\d+)*)$/)
-  groups = ([match[1]] * 5).join('?').split /\.+/
+  groups = ([match[1]] * 5).join('?').split(/\.+/).map &:chars
   lengths = match[2].split(',').map(&:to_i) * 5
+  arrangements = 0
 
-  while groups.first.match(/^#+$/) || groups.first.match(/^?+$/) && groups.first.length < lengths.first
-    raise "Unexpected group length mismatch on line #{line}" unless groups.shift == '#' * lengths.shift while groups.first.match(/^#+$/)
-    groups.shift while groups.first.match(/^?+$/) && groups.first.length < lengths.first
-  end
-  while groups.last.match(/^#+$/) || groups.last.match(/^?+$/) && groups.last.length < lengths.last
-    raise "Unexpected group length mismatch on line #{line}" unless groups.pop == '#' * lengths.pop while groups.last.match(/^#+$/)
-    groups.pop while groups.last.match(/^?+$/) && groups.last.length < lengths.last
-  end
+  to_visit = DedupQueue.new groups, lengths
+  until to_visit.empty?
+    groups, lengths, quantity = to_visit.pop
 
-  groups.each_with_index.flat_map do |group, group_idx|
-    group.chars.each_with_index.select do |char, _|
-      char == '?'
-    end.map do |_, char_idx|
-      [group_idx, char_idx]
+    changed = true
+    while changed
+      changed = false
+      groups.shift and changed = true  while groups.first&.empty?
+      groups.shift and lengths.shift and changed = true  while !groups.empty? && groups.first.include?('#') && groups.first.length == lengths.first
+      groups.shift and changed = true  while !groups.empty? && !lengths.empty? && !groups.first.include?('#') && groups.first.length < lengths.first
+
+      groups.first.shift and changed = true  while !groups.empty? && !lengths.empty? && groups.first.first == '?' && groups.first[lengths.first] == '#'
+      groups.first.shift lengths.shift + 1 and changed = true  while !groups.empty? && !lengths.empty? && groups.first.first == '#' && groups.first.length > lengths.first && groups.first[lengths.first] == '?'
+
+      groups.pop and changed = true while groups.last&.empty?
+      groups.pop and lengths.pop and changed = true while !groups.empty? && groups.last.include?('#') && groups.last.length == lengths.last
+      groups.pop and changed = true while !groups.empty? && !lengths.empty? && !groups.last.include?('#') && groups.last.length < lengths.last
+
+      groups.last.pop and changed = true while !groups.empty? && !lengths.empty? && groups.last.last == '?' && groups.last[-1 * (lengths.last + 1)] == '#'
+      groups.last.pop lengths.pop + 1 and changed = true while !groups.empty? && !lengths.empty? && groups.last.last == '#' && groups.last.length > lengths.last && groups.last[-1 * (lengths.last + 1)] == '?'
     end
-  end.combination(lengths.sum - groups.sum {|group| group.count '#'}).sum do |assumptions|
-    assigned = groups.map &:dup
-    assumptions.each do |group_idx, char_idx|
-      assigned[group_idx][char_idx] = '#'
-    end
-    if assigned.flat_map do |group|
-      group.sub(/^\?*/, '').sub(/\?*$/, '').split /\?+/
-    end.map(&:length) == lengths
-      1
+
+    next if lengths.empty? && groups.any? {|group| group.include? '#'}
+    arrangements += quantity and next if lengths.empty? || groups.map(&:length) == lengths
+
+    next if groups.sum(&:length) <= lengths.sum
+
+    next if groups.first.length < lengths.first && groups.first.include?('#')
+    next if groups.last.length < lengths.last && groups.last.include?('#')
+
+    next if groups.first.length > lengths.first && groups.first[lengths.first] == '#'
+    next if groups.last.length > lengths.last && groups.last[-1 * (lengths.last + 1)] == '#'
+
+    if groups.first.length <= lengths.first
+      raise if groups.first.include? '#'
+      to_visit.push groups[1..-1].map(&:dup), lengths.dup, quantity unless groups[1..-1].sum(&:length) < lengths.sum
     else
-      0
+      raise if groups.first.first == '#'
+      to_visit.push [groups.first[1..-1], *groups[1..-1].map(&:dup)], lengths.dup, quantity unless groups.sum(&:length) == lengths.sum
+    end
+
+    if groups.first.length == lengths.first
+      to_visit.push groups[1..-1].map(&:dup), lengths[1..-1], quantity
+    elsif groups.first.length > lengths.first
+      to_visit.push [groups.first[(lengths.first + 1)..-1], *groups[1..-1].map(&:dup)], lengths[1..-1], quantity unless groups.first[lengths.first] == '#'
     end
   end
+
+  arrangements
 }
