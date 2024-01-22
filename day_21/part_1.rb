93d10f3be2feeb5f3f41c8869c59236c2ef95f66
Dan Barry <dan@bakineggs.com>
2024-01-22 00:40:27 -0800

    day 21 part 1

---
+++ day_21/part_1.rb
@@ -0,0 +1,29 @@
+require 'set'
+
+lines = File.readlines ARGV[0], chomp: true
+
+raise 'Expected all lines to have the same length' unless lines.all? {|line| line.length == lines.first.length}
+raise 'Expected all lines to only contain . # and S' unless lines.all? {|line| line.match /^[.#S]+$/}
+raise 'Expected exactly one S in the input' unless lines.sum {|line| line.count 'S'} == 1
+
+STEPS = ARGV[1].to_i
+
+start_row = lines.find_index {|line| line.include? 'S'}
+start_col = lines[start_row].index 'S'
+to_explore = [[start_row, start_col, 0]]
+explored = Set.new
+end_points = 0
+
+until to_explore.empty?
+  row, col, steps = to_explore.shift
+  next if explored.include? [row, col]
+  explored.add [row, col]
+  end_points += 1 if steps % 2 == STEPS % 2
+  next if steps == STEPS
+  [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]].each do |row, col|
+    next if row == -1 || row == lines.length || col == -1 || col == lines.first.length || lines[row][col] == '#'
+    to_explore.push [row, col, steps + 1]
+  end
+end
+
+puts end_points
