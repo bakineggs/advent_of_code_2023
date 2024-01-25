a98b146e01a4ddf3b46bea21db8140e922a8105c
Dan Barry <dan@bakineggs.com>
2024-01-25 04:47:36 -0800

    day 23 part 2 using reduced adjacency list

--- day_23/part_2.rb
+++ day_23/part_2.rb
@@ -9,23 +9,47 @@ raise 'Expected exit in the bottom right corner' unless lines.last.match /^#+\.\
 
 max_row, max_col = lines.length - 1, lines.first.length - 1
 
-longest, to_explore, explored = nil, [[0, 1, Set.new]], Set.new
+adjacency, to_explore, explored = {}, [[0, 1]], Set.new
 until to_explore.empty?
-  row, col, visited = to_explore.pop
-  next if explored.include? [row, col, visited]
-  explored.add [row, col, visited]
-  visited += [[row, col]]
-
-  if row == max_row
-    longest = visited.length if longest.nil? || longest < visited.length
-    next
-  end
+  row, col = to_explore.pop
+  next if row == max_row
+  next if explored.include? [row, col]
+  explored.add [row, col]
 
   [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]].each do |r, c|
     next if r == 0 || c == 0 || c == max_col || lines[r][c] == '#'
-    next if visited.include? [r, c]
-    to_explore.push [r, c, visited]
+    next if explored.include? [r, c]
+    (adjacency[[row, col]] ||= []).push [[r, c], 1]
+    (adjacency[[r, c]] ||= []).push [[row, col], 1]
+    to_explore.push [r, c]
   end
 end
 
-puts longest - 1
+while n = adjacency.keys.find {|n| adjacency[n].length == 2}
+  (n1, w1), (n2, w2) = adjacency[n]
+  raise unless adjacency[n1].include? [n, w1]
+  raise unless adjacency[n2].include? [n, w2]
+  adjacency[n1].delete [n, w1]
+  adjacency[n2].delete [n, w2]
+  adjacency.delete n
+  adjacency[n1].push [n2, w1 + w2]
+  adjacency[n2].push [n1, w1 + w2]
+end
+
+longest, to_explore = nil, [[[0, 1], Set.new, 0]]
+until to_explore.empty?
+  n1, visited, length = to_explore.pop
+  visited += [n1]
+
+  if n1[0] == max_row
+    longest = length if longest.nil? || longest < length
+    next
+  end
+
+  adjacency[n1].each do |n2, weight|
+    next if visited.include? n2
+    to_explore.push [n2, visited, length + weight]
+  end
+end
+
+puts longest
