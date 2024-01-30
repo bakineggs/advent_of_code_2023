4c7138ffd5c62e562f67d6dde3939e4ce9ea0d62
Dan Barry <dan@bakineggs.com>
2024-01-29 22:22:26 -0800

    day 24 part 2 faster, but still not fast enough

--- day_24/part_2.rb
+++ day_24/part_2.rb
@@ -3,35 +3,38 @@ hail = File.readlines(ARGV[0]).map do |line|
   match[1..6].map &:to_i
 end
 
-i = 0
+x0, y0, z0, dx0, dy0, dz0 = hail.pop
+x1, y1, z1, dx1, dy1, dz1 = hail.pop
+x2, y2, z2, dx2, dy2, dz2 = hail.pop
+dx1x2, dy1y2, dz1z2 = x1 - x2, y1 - y2, z1 - z2
+t0 = 0
 loop do
-  i += 1
-  (0...i).flat_map {|i2| [[i, i2], [i2, i]]}.each do |t1, t2|
-    x1, y1, z1 = hail[0][0..2].zip(hail[0][3..5]).map {|p, d| p + d * t1}
-    x2, y2, z2 = hail[1][0..2].zip(hail[1][3..5]).map {|p, d| p + d * t2}
-    dx12, dy12, dz12 = x2 - x1, y2 - y1, z2 - z1
-    dt = t2 - t1
-    next unless dx12 % dt == 0 && dy12 % dt == 0 && dz12 % dt == 0
-    next unless hail[2..].all? do |x2, y2, z2, dx2, dy2, dz2|
-      (dy12 * dz2 - dz12 * dy2) * (x1 - x2) + (dz12 * dx2 - dx12 * dz2) * (y1 - y2) + (dx12 * dy2 - dy12 * dx2) * (z1 - z2) == 0
-    end
-    if t1 < t2
-      x2, y2, z2 = hail[1][0..2].zip(hail[1][3..5]).map {|p, d| p + d * t2}
-    else
-      t1, t2 = t2, t1
-      x2, y2, z2 = x1, y1, z1
-      x1, y1, z1 = hail[1][0..2].zip(hail[1][3..5]).map {|p, d| p + d * t1}
-    end
-    dt = t2 - t1
-    raise unless (x2 - x1) % dt == 0
-    raise unless (y2 - y1) % dt == 0
-    raise unless (z2 - z1) % dt == 0
-    dx = (x2 - x1) / dt
-    x = x1 - dx * t1
-    next unless hail[2..].all? do |x2, y2, z2, dx2, dy2, dz2|
-      (x - x2) / (dx2 - dx) > 0
-    end
-    puts x + y1 - (y2 - y1) / dt * t1 + z1 - (z2 - z1) / dt * t1
-    exit 0
+  t0 += 1
+  x0 += dx0 and y0 += dy0 and z0 += dz0
+  xn = (y0 - y1) * dz1 - (z0 - z1) * dy1
+  yn = (z0 - z1) * dx1 - (x0 - x1) * dz1
+  zn = (x0 - x1) * dy1 - (y0 - y1) * dx1
+
+  dp = xn * dx2 + yn * dy2 + zn * dz2
+  next if dp == 0
+  t2 = dx1x2 * xn + dy1y2 * yn + dz1z2 * zn
+  next unless t2 % dp == 0
+  t2 /= dp
+  xp, yp, zp = x2 + dx2 * t2, y2 + dy2 * t2, z2 + dz2 * t2
+  dxp, dyp, dzp = xp - x0, yp - y0, zp - z0
+
+  next unless hail.all? do |x, y, z, dx, dy, dz|
+    (dy * dzp - dz * dyp) * (x - x0) + (dz * dxp - dx * dzp) * (y - y0) + (dx * dyp - dy * dxp) * (z - z0) == 0
   end
+
+  dx, dy, dz = (x0 - xp) / (t0 - t2), (y0 - yp) / (t0 - t2), (z0 - zp) / (t0 - t2)
+  x, y, z = xp - t2 * dx, yp - t2 * dy, zp - t2 * dz
+
+  next unless hail.all? do |xh, _, _, dxh|
+    xd, dxd = xh - x, dx - dxh
+    xd % dxd == 0 && xd / dxd > 0
+  end
+
+  puts x + y + z
+  exit 0
 end
